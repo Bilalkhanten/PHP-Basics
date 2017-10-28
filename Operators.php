c0e33d819727d32b06448c44dc4ee75ee747f6fc
Bilal Khan <Bilalkhanten@gmail.com>
2017-10-28 01:46:53 -0700

    Create Operators.php

---
+++ Operators.php
@@ -0,0 +1,13 @@
+<?php
+// Bereken de rest bij gehele deling van 10 door 3.
+// Bereken de 5e macht van 2.
+$var_a = "123";
+$var_b = 123;
+// Vervang de true in de if-voorwaarden zodat het klopt wat er staat:
+if(true) {
+    echo '$var_a en $var_b zijn aan elkaar gelijk maar zijn niet van hetzelfde type.';
+}
+if(true) {
+    echo '$var_a en $var_b zijn aan elkaar gelijk en van hetzelfde type.';
+}
+?>
