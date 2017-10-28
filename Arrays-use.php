ae5cbcd462f3be66096536992409f2ff84027e89
Bilal Khan <Bilalkhanten@gmail.com>
2017-10-28 01:47:38 -0700

    Create Arrays-use.php

---
+++ Arrays-use.php
@@ -0,0 +1,7 @@
+<?php
+// Maak een Array met 3 namen van medeleerlingen.
+// Itereer er over met een for-loop. Print ze af in een HTML ongenummerde lijst.
+// Itereer er over met een foreach-loop. Print ze af in HTML genummerde lijst.
+$naam = "Willy";
+// Itereer met een for-loop over de variable $naam alsof het een array was.
+?>
